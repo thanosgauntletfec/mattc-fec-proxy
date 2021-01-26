60cc849123916d4c00dea5f7851a129d9eb20767
Matt Carlos <catmatlos@icloud.com>
2021-01-25 16:36:36 -0800

    test push to new-feature

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const { createProxyMiddleware }  = require('http-proxy-middleware');
+const path = require('path')
+
+
+const app = express();
+
+const PORT = 3080;
+const HOST = "localhost";
+
+app.use(express.static(path.join(__dirname, './public')));
+// app.get('/info', (req, res) => {
+//   res.send('this is a proxy service for fec thanos')
+// });
+
+const matt = {
+  target: "http://localhost:2080/",
+  changeOrigin: true,
+  pathRewrite: {
+    [`^/matt`]: '',
+},
+}
+
+const chase = {
+  target: "http://localhost:3000/",
+  changeOrigin: true,
+  pathRewrite: {
+    [`^/chase`]: '',
+  },
+}
+
+const jason = {
+  target: "http://localhost:8080/",
+  changeOrigin: true,
+  pathRewrite: {
+    [`^/jason`]: '',
+  },
+}
+
+app.use('/chase/*', createProxyMiddleware(chase));
+app.use('/matt/*', createProxyMiddleware(matt));
+app.use ('/jason/*', createProxyMiddleware(jason))
+
+
+
+
+app.listen(PORT, "localhost", () => {
+  console.log(`starting proxy at ${HOST}:${PORT}`)
+})
+
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  entry: [
+    path.resolve(__dirname, './cmanouk-fec-service/client/index.js'),
+    path.resolve(__dirname, './jpanay-fec-service/client/app.js'),
+    path.resolve(__dirname, './mattc-fec-service/client/index.js')
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react', '@babel/preset-env'],
+          }
+        }
+      },
+      {
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+};
