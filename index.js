a09b6d03cd14843cf61e902dfbbbc388ad49b434
Matt Carlos <catmatlos@icloud.com>
2021-01-26 19:32:36 -0800

    reverse proxy set up

--- index.js
+++ index.js
@@ -1,45 +1,51 @@
 const express = require('express');
 const { createProxyMiddleware }  = require('http-proxy-middleware');
 const path = require('path')
-
+const cors = require('cors')
 
 const app = express();
 
 const PORT = 3080;
 const HOST = "localhost";
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(cors({
+  origin: 'http://localhost:2080/',
+  optionsSuccessStatus: 200
+}))
+app.use(express.static('./public'));
+
+
 // app.get('/info', (req, res) => {
 //   res.send('this is a proxy service for fec thanos')
 // });
 
-const matt = {
-  target: "http://localhost:2080/",
-  changeOrigin: true,
-  pathRewrite: {
-    [`^/matt`]: '',
-},
-}
+// const matt = {
+//   target: "http://localhost:2080/",
+//   changeOrigin: true,
+//   pathRewrite: {
+//     [`^/matt`]: '',
+// },
+// }
 
-const chase = {
-  target: "http://localhost:3000/",
-  changeOrigin: true,
-  pathRewrite: {
-    [`^/chase`]: '',
-  },
-}
+// const chase = {
+//   target: "http://localhost:3000/",
+//   changeOrigin: true,
+//   pathRewrite: {
+//     [`^/chase`]: '',
+//   },
+// }
 
-const jason = {
-  target: "http://localhost:8080/",
-  changeOrigin: true,
-  pathRewrite: {
-    [`^/jason`]: '',
-  },
-}
+// const jason = {
+//   target: "http://localhost:8080/",
+//   changeOrigin: true,
+//   pathRewrite: {
+//     [`^/jason`]: '',
+//   },
+// }
 
-app.use('/chase/*', createProxyMiddleware(chase));
-app.use('/matt/*', createProxyMiddleware(matt));
-app.use ('/jason/*', createProxyMiddleware(jason))
+// app.use('/chase/*', createProxyMiddleware(chase));
+// app.use('/matt/*', createProxyMiddleware(matt));
+// app.use ('/jason/*', createProxyMiddleware(jason))
 
 
 
