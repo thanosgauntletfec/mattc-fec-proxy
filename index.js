e5f8dc5656968525f057b93d110693e967305040
Matt Carlos <catmatlos@icloud.com>
2021-01-28 15:39:17 -0800

    changes to script tags

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = 3080;
 const HOST = "localhost";
 
 app.use(cors({
-  origin: 'http://ec2-54-183-180-104.us-west-1.compute.amazonaws.com::2080/',
+  origin: 'http://ec2-54-183-180-104.us-west-1.compute.amazonaws.com:2080/',
   optionsSuccessStatus: 200
 }))
 app.use(express.static('./public'));
