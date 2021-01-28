3f953717ec5522630fd624af74cab00d744b0d77
Matt Carlos <catmatlos@icloud.com>
2021-01-28 15:24:51 -0800

    changes to script tags

--- index.js
+++ index.js
@@ -9,44 +9,10 @@ const PORT = 3080;
 const HOST = "localhost";
 
 app.use(cors({
-  origin: 'http://localhost:2080/',
+  origin: 'http://ec2-54-183-180-104.us-west-1.compute.amazonaws.com::2080/',
   optionsSuccessStatus: 200
 }))
 app.use(express.static('./public'));
-
-
-// app.get('/info', (req, res) => {
-//   res.send('this is a proxy service for fec thanos')
-// });
-
-// const matt = {
-//   target: "http://localhost:2080/",
-//   changeOrigin: true,
-//   pathRewrite: {
-//     [`^/matt`]: '',
-// },
-// }
-
-// const chase = {
-//   target: "http://localhost:3000/",
-//   changeOrigin: true,
-//   pathRewrite: {
-//     [`^/chase`]: '',
-//   },
-// }
-
-// const jason = {
-//   target: "http://localhost:8080/",
-//   changeOrigin: true,
-//   pathRewrite: {
-//     [`^/jason`]: '',
-//   },
-// }
-
-// app.use('/chase/*', createProxyMiddleware(chase));
-// app.use('/matt/*', createProxyMiddleware(matt));
-// app.use ('/jason/*', createProxyMiddleware(jason))
-
 
 
 
