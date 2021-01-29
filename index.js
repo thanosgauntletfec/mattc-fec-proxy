f8505e9252c7164b7119c16229805805a84f458f
Matt Carlos <catmatlos@icloud.com>
2021-01-28 16:09:43 -0800

    updating paths

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const cors = require('cors')
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
-const PORT = 3080;
+const PORT = process.env.PORT || 3080;
 
 
-app.use(cors({
-  origin: 'http://ec2-54-183-180-104.us-west-1.compute.amazonaws.com:2080/',
-  optionsSuccessStatus: 200
-}))
+// app.use(cors({
+//   origin: 'http://ec2-54-183-180-104.us-west-1.compute.amazonaws.com:2080/',
+//   optionsSuccessStatus: 200
+// }))
 app.use(express.static(path.join(__dirname, './public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
