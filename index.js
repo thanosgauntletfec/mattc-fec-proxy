50d91d0d347f4e6c23daa8bf708157f4e531a274
Matt Carlos <catmatlos@icloud.com>
2021-01-28 16:13:32 -0800

    updating paths

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3080;
 
 
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
