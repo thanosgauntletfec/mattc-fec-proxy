ab49fbe774efccbc0d67ac757760dce2cc2de18c
Matt Carlos <catmatlos@icloud.com>
2021-01-28 16:04:04 -0800

    updating paths

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors')
-
+const path = require('path');
 const app = express();
 
 const PORT = 3080;
@@ -10,7 +10,7 @@ app.use(cors({
   origin: 'http://ec2-54-183-180-104.us-west-1.compute.amazonaws.com:2080/',
   optionsSuccessStatus: 200
 }))
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, './public')));
 
 
 
